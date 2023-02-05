20f9e64b4d79d3720f0013a08437ee80e9baa0ff
hectpumhui <forsoctask@gmail.com>
2023-02-05 20:48:11 +0200

    create task model

--- blogik/task/models.py
+++ blogik/task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255, blank=True) # can be empty
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=True)
